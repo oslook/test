4a2ccd4b1a771fa725886e564c1228588212469d
osLook <oslook@users.noreply.github.com>
2014-11-14 23:19:33 +0800

    Update share.js

--- js/share.js
+++ js/share.js
@@ -1,9 +1,9 @@
 /////step2 url/////
-var shareDomains = ["wt.oslook.com"];
+var shareDomains = ["106.186.124.50"];
 var shareDomain = this.shareDomains[parseInt(Math.random() * this.shareDomains.length)];
 
-dataForWeixin.url = "http://"+shareDomain+"/test/"+spid+"/?uuid="+ID;
-
+//dataForWeixin.url = "http://Q"+ID+"."+shareDomain+"/test/"+spid+"/";
+dataForWeixin.url = "http://"+shareDomain+"/"+ID+"/test/"+spid+"/";
 
 /////step3 url/////
 //var shortdata='type=jsonp&mode=0&long=http://wx.oslook.com/test/'+ spid + '/index.html?uuid='+ID;
